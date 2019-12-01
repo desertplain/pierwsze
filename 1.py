4b1ad1579d267ce1fd5645b05031f09eb5ff00e0
desertplain <darekkob@op.pl>
2019-12-01 14:12:33 +0100

    pierwszy plik

---
+++ 1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+print("helou")
+x=input("wprowadz liczbe:    ")
+print "wpisales:  ",x
+if x<-10: 
+    print("liczba mniejsza od -10")
+elif x>20:
+    print("liczba wieksza od 20")
+else:
+    print("liczba pomiedzy")
+
